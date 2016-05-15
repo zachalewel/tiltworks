88e7dd3feafd064cd7cca13eb863aa056404096a
Zach Alewel <zachalewel@gmail.com>
2016-05-15 14:14:50 -0600

    adds routes for “about” and “contact”

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'admin#index'
+
+  get 'about' => 'admin#about'
+
+  get 'contact' => 'admin#contact'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
