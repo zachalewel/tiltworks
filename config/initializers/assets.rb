c999c1961053b5c478fc35b7d2a71bba68a38b99
Zach Alewel <zachalewel@gmail.com>
2016-05-21 20:31:11 -0600

    Adds background image to contact page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,7 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 Rails.application.config.assets.paths << Rails.root.join('/app/assets/fonts')
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "videos")
-Rails.application.config.assets.precompile += %w( contact.css )
+Rails.application.config.assets.precompile += %w( contact.css.scss )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
